1aa8a5df304e2e6754bb086e6e328576fa7db0a6
hamidshariat <hamid13841147@gmail.com>
2023-10-11 15:19:11 +0330

    Sort codes

--- script.js
+++ script.js
@@ -1,29 +1,27 @@
-const note = document.querySelector('.nowplaying');
-const keys = document.querySelectorAll('.key');
-const hints = document.querySelectorAll('.hints');
+const note = document.querySelector(".nowplaying");
+const keys = document.querySelectorAll(".key");
+const hints = document.querySelectorAll(".hints");
 
-window.addEventListener('keydown' , function(e) {
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+window.addEventListener("keydown", function (e) {
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
 
-    if(! key) return;
+  if (!key) return;
 
-    const keyNote = key.getAttribute('data-note');
- 
-    key.classList.add('playing');
-    note.innerHTML = keyNote;
-    audio.currentTime = 0;
-    audio.play();
+  const keyNote = key.getAttribute("data-note");
+
+  key.classList.add("playing");
+  note.innerHTML = keyNote;
+  audio.currentTime = 0;
+  audio.play();
 });
 
 function removeTransition() {
-    this.classList.remove('playing')
+  this.classList.remove("playing");
 }
 
+hints.forEach(function (elm, index) {
+  elm.setAttribute("style", `transition-delay : ${index * 50}ms`);
+});
 
-hints.forEach(function(elm , index) {
-    // elm.style = `transition-delay : ${index * 50}ms`
-    elm.setAttribute('style' , `transition-delay : ${index * 50}ms`)
-})
-
-keys.forEach(key => key.addEventListener('transitionend' , removeTransition))
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
